a3fd929f790ffb414088bc8dedf00818d93a6c85
ketsai <kelvin.tsai@stonybrook.edu>
2019-02-17 10:11:47 +0000

    implemented random AI moves

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.get('/ttt', function(req, res, next){
 });
 
 app.post('/ttt', function(req, res, next){
+  //fill in given name and date
   if(req.body.name != ''){
     var date = new Date();
     var y = date.getFullYear();
@@ -31,14 +32,34 @@ app.post('/ttt', function(req, res, next){
   }
 });
 
-app.post('/ttt/play', function(req, res, next){
-  var grid = req.body.grid;
-
-  if(grid[0] == grid[1] && grid[1] == grid[2] && grid[0] != " "){
-    winner = grid[0];
-  }
-  console.log("winner was:" + req.body.winner);
-  res.json({'uhh':'pls work','winner':'pongo'});
+app.post('/ttt/play', function (req, res, next) {
+    //check for a winner and return json
+    var grid = JSON.parse(req.body.grid);
+    var winner = ' ';
+    for (var i = 0; i < 9; i++) { //game should continue if there's at least one empty space
+        if (grid[i] == " ") {
+            winner = '';
+        }
+    }
+    //unless someone has won
+    if (grid[0] == grid[1] && grid[1] == grid[2] && grid[0] != " ") { //r0
+        winner = grid[0];
+    } else if (grid[3] == grid[4] && grid[4] == grid[5] && grid[3] != " ") { //r1
+        winner = grid[3];
+    } else if (grid[6] == grid[7] && grid[7] == grid[8] && grid[6] != " ") { //r2
+        winner = grid[6];
+    } else if (grid[0] == grid[3] && grid[3] == grid[6] && grid[0] != " ") { //c0
+        winner = grid[0];
+    } else if (grid[1] == grid[4] && grid[4] == grid[7] && grid[1] != " ") { //c1
+        winner = grid[1];
+    } else if (grid[2] == grid[5] && grid[5] == grid[8] && grid[2] != " ") { //c2
+        winner = grid[2];
+    } else if (grid[0] == grid[4] && grid[4] == grid[8] && grid[0] != " ") { //top left to bottom right
+        winner = grid[0];
+    } else if (grid[2] == grid[4] && grid[4] == grid[6] && grid[2] != " ") { //top right to bottom left
+        winner = grid[2];
+    }
+    res.json({grid: grid, winner: winner});
 });
 
 //start server
--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,145 +1,180 @@
 var gridArray = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
+function botTurn(serverResponse) {
+  if (serverResponse.winner == ' ') {
+    $("#winner").html("Tie game");
+  } else if (serverResponse.winner != '') {
+    $("#winner").html(serverResponse.winner + " wins!");
+  } else {
+    var randomCell;
+    while (gridArray[randomCell] != " ") {
+      randomCell = Math.floor(Math.random() * 9);
+      console.log("randomly selected cell: " + randomCell.toString());
+    }
+    $("#cell" + randomCell.toString()).html("O");
+    gridArray[randomCell] = "O";
+    $.ajax({
+      data: { grid: JSON.stringify(gridArray) },
+      url: "/ttt/play",
+      method: "POST",
+      success: function (response) {
+        if(response.winner != ''){
+          $("#winner").html(response.winner + " wins!");
+        }
+      }
+    });
+  }
+}
+
 $(document).ready(function(){
   $("#cell0").click(function(){
-    if(gridArray[0] == " "){
-      gridArray[0] = "X";
-      $.ajax({
-        data:{"grid": gridArray},
-        url: "/ttt/play",
-        method: "POST",
-        success: function(response){
-          $("#cell0").html("X");
-        }
-      });
-    }
+	if(gridArray[0] == " "){
+	  gridArray[0] = "X";
+	  $.ajax({
+		data:{grid: JSON.stringify(gridArray)},
+		url: "/ttt/play",
+		method: "POST",
+		success: function(response){
+		  $("#cell0").html("X");
+		  botTurn(response);
+		}
+	  });
+	}
   });
 });
 
 $(document).ready(function(){
   $("#cell1").click(function(){
-    if(gridArray[1] == " "){
-      gridArray[1] = "X";
-      $.ajax({
-        data:{"grid": gridArray},
-        url: "/ttt/play",
-        method: "POST",
-        success: function(response){
-          $("#cell1").html("X");
-        }
-      });
-    }
+	if(gridArray[1] == " "){
+	  gridArray[1] = "X";
+	  $.ajax({
+          data:{grid: JSON.stringify(gridArray)},
+		url: "/ttt/play",
+		method: "POST",
+		success: function(response){
+		  $("#cell1").html("X");
+		  botTurn(response);
+		}
+	  });
+	}
   });
 });
 
 $(document).ready(function(){
   $("#cell2").click(function(){
-    if(gridArray[2] == " "){
-      gridArray[2] = "X";
-      $.ajax({
-        data:{"grid": gridArray},
-        url: "/ttt/play",
-        method: "POST",
-        success: function(response){
-          $("#cell2").html("X");
-        }
-      });
-    }
+	if(gridArray[2] == " "){
+	  gridArray[2] = "X";
+	  $.ajax({
+		data:{grid: JSON.stringify(gridArray)},
+		url: "/ttt/play",
+		method: "POST",
+		success: function(response){
+		  $("#cell2").html("X");
+		  botTurn(response);
+		}
+	  });
+	}
   });
 });
 
 $(document).ready(function(){
   $("#cell3").click(function(){
-    if(gridArray[3] == " "){
-      gridArray[3] = "X";
-      $.ajax({
-        data:{"grid": gridArray},
-        url: "/ttt/play",
-        method: "POST",
-        success: function(response){
-          $("#cell3").html("X");
-        }
-      });
-    }
+	if(gridArray[3] == " "){
+	  gridArray[3] = "X";
+	  $.ajax({
+		data:{grid: JSON.stringify(gridArray)},
+		url: "/ttt/play",
+		method: "POST",
+		success: function(response){
+		  $("#cell3").html("X");
+		  botTurn(response);
+		}
+	  });
+	}
   });
 });
 
 $(document).ready(function(){
   $("#cell4").click(function(){
-    if(gridArray[4] == " "){
-      gridArray[4] = "X";
-      $.ajax({
-        data:{"grid": gridArray},
-        url: "/ttt/play",
-        method: "POST",
-        success: function(response){
-          $("#cell4").html("X");
-        }
-      });
-    }
+	if(gridArray[4] == " "){
+	  gridArray[4] = "X";
+	  $.ajax({
+		data:{grid: JSON.stringify(gridArray)},
+		url: "/ttt/play",
+		method: "POST",
+		success: function(response){
+		  $("#cell4").html("X");
+		  botTurn(response);
+		}
+	  });
+	}
   });
 });
 
 $(document).ready(function(){
   $("#cell5").click(function(){
-    if(gridArray[5] == " "){
-      gridArray[5] = "X";
-      $.ajax({
-        data:{"grid": gridArray},
-        url: "/ttt/play",
-        method: "POST",
-        success: function(response){
-          $("#cell5").html("X");
-        }
-      });
-    }
+	if(gridArray[5] == " "){
+	  gridArray[5] = "X";
+	  $.ajax({
+		data:{grid: JSON.stringify(gridArray)},
+		url: "/ttt/play",
+		method: "POST",
+		success: function(response){
+		  $("#cell5").html("X");
+		  botTurn(response);
+		}
+	  });
+	}
   });
 });
 
 $(document).ready(function(){
   $("#cell6").click(function(){
-    if(gridArray[6] == " "){
-      gridArray[6] = "X";
-      $.ajax({
-        data:{"grid": gridArray},
-        url: "/ttt/play",
-        method: "POST",
-        success: function(response){
-          $("#cell6").html("X");
-        }
-      });
-    }
+	if(gridArray[6] == " "){
+	  gridArray[6] = "X";
+	  $.ajax({
+		data:{grid: JSON.stringify(gridArray)},
+		url: "/ttt/play",
+		method: "POST",
+		success: function(response){
+		  $("#cell6").html("X");
+		  botTurn(response);
+		}
+	  });
+	}
   });
 });
 
 $(document).ready(function(){
   $("#cell7").click(function(){
-    if(gridArray[7] == " "){
-      gridArray[7] = "X";
-      $.ajax({
-        data:{"grid": gridArray},
-        url: "/ttt/play",
-        method: "POST",
-        success: function(response){
-          $("#cell7").html("X");
-        }
-      });
-    }
+	if(gridArray[7] == " "){
+	  gridArray[7] = "X";
+	  $.ajax({
+		data:{grid: JSON.stringify(gridArray)},
+		url: "/ttt/play",
+		method: "POST",
+		success: function(response){
+		  $("#cell7").html("X");
+		  botTurn(response);
+		}
+	  });
+	}
   });
 });
 
 $(document).ready(function(){
   $("#cell8").click(function(){
-    if(gridArray[8] == " "){
-      gridArray[8] = "X";
-      $.ajax({
-        data:{"grid": gridArray},
-        url: "/ttt/play",
-        method: "POST",
-        success: function(response){
-          $("#cell8").html("X");
-        }
-      });
-    }
+	if(gridArray[8] == " "){
+	  gridArray[8] = "X";
+	  $.ajax({
+		data:{grid: JSON.stringify(gridArray)},
+		url: "/ttt/play",
+		method: "POST",
+		success: function(response){
+		  $("#cell8").html("X");
+		  botTurn(response);
+		}
+	  });
+	}
   });
 });
