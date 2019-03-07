b7d246ae767f9754b9343472ed16612e3f40699a
ketsai <kelvin.tsai@stonybrook.edu>
2019-03-06 22:07:34 -0500

    Update rabbitmq.js

--- routes/rabbitmq.js
+++ routes/rabbitmq.js
@@ -3,9 +3,7 @@ var router = express.Router();
 var amqp = require('amqplib/callback_api');
 
 router.post('/listen', function (req, res, next) {
-	console.log(req.body);
 	var keys = req.body.keys;
-	//console.log(keys);
 	amqp.connect('amqp://localhost', function (err, conn) {
 		conn.createChannel(function (err, ch) {
 			var ex = 'hw3';
@@ -16,6 +14,7 @@ router.post('/listen', function (req, res, next) {
 				});
 				ch.consume(q.queue, function (msg) {
 					console.log("Sent msg : '" + msg.content.toString() + "' to " + msg.fields.routingKey);
+					setTimeout(function () { conn.close() }, 500);
 					res.json({ msg: msg.content.toString() });
 				});
 			});
@@ -30,10 +29,8 @@ router.post('/speak', function (req, res, next) {
 		conn.createChannel(function (err, ch) {
 			var ex = 'hw3';
 			ch.publish(ex, key, new Buffer(msg));
-			console.log("Sent msg: " + msg);
-			console.log("to key: " + key);
 		});
-		setTimeout(function () { conn.close(); process.exit(0) }, 500);
+		setTimeout(function () { conn.close() }, 500);
 	});
 	res.end();
 });
