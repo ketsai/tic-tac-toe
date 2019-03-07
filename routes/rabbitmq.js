47af52b12610851ad5f94accdf06b4583ddc8930
ketsai <kelvin.tsai@stonybrook.edu>
2019-03-07 01:43:50 -0500

    Update rabbitmq.js

--- routes/rabbitmq.js
+++ routes/rabbitmq.js
@@ -4,22 +4,25 @@ var amqp = require('amqplib/callback_api');
 
 router.post('/listen', function (req, res, next) {
 	var keys = req.body.keys;
-	amqp.connect('amqp://localhost', function (err, conn) {
-		conn.createChannel(function (err, ch) {
-			var ex = 'hw3';
-			ch.assertExchange(ex, 'direct', { durable: false });
-			ch.assertQueue('', { exclusive: true }, function (err, q) {
-				keys.forEach(function (key) {
-					ch.bindQueue(q.queue, ex, key);
-				});
-				ch.consume(q.queue, function (msg) {
-					console.log("Sent msg : '" + msg.content.toString() + "' to " + msg.fields.routingKey);
-					setTimeout(function () { conn.close() }, 500);
-					res.json({ msg: msg.content.toString() });
+	if (keys.length > 0) {
+		amqp.connect('amqp://localhost', function (err, conn) {
+			conn.createChannel(function (err, ch) {
+				var ex = 'hw3';
+				ch.assertExchange(ex, 'direct', { durable: false });
+				ch.assertQueue('', { exclusive: true }, function (err, q) {
+					keys.forEach(function (key) {
+						ch.bindQueue(q.queue, ex, key);
+						console.log("Listening on key=" + key);
+					});
+					ch.consume(q.queue, function (msg) {
+						console.log("Sent msg : '" + msg.content.toString() + "' to " + msg.fields.routingKey);
+						res.json({ msg: msg.content.toString() });
+						setTimeout(function () { conn.close() }, 500);
+					}, { noAck: true });
 				});
 			});
 		});
-	});
+	}
 });
 
 router.post('/speak', function (req, res, next) {
@@ -28,6 +31,7 @@ router.post('/speak', function (req, res, next) {
 	amqp.connect('amqp://localhost', function (err, conn) {
 		conn.createChannel(function (err, ch) {
 			var ex = 'hw3';
+			console.log("Speaking: '" + msg + "' to key=" + key);
 			ch.publish(ex, key, new Buffer(msg));
 		});
 		setTimeout(function () { conn.close() }, 500);
